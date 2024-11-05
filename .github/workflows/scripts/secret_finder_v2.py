035a0f16cae788789be0c2df6d3d5b21349c356d
luk295 <lcastellanoostapczuk@danielcastelao.org>
2024-11-05 16:50:58 +0100

    subida scipts

--- .github/workflows/scripts/secret_finder_v2.py
+++ .github/workflows/scripts/secret_finder_v2.py
@@ -7,14 +7,18 @@
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/setting-exit-codes-for-actions
 
 
-
-if ( sys.exit(n) != 0 )
-
+import sys
+import subprocess
 
 def main():
-	cat ./* | grep "pass"
-	if ( sys.exit(n) != 0 )
-    print("Todavía no estoy listo!")
-
-if __name__ == "__main__":
-    main()
+	ruta = sys.argv[1]
+	contenido = sys.argv[2]
+	result = subprocess.run(['egrep', '-R', contenido, ruta], capture_output=True, text=True)
+	print(result)
+	if (result.returncode == 0):
+		sys.exit(1)
+if _name_ == "_main_":
+	if len(sys.argv) != 3:
+		print("Uso: python secret_finder_2.py <ruta> <contenido>")
+		sys.exit(1)
+	main()
